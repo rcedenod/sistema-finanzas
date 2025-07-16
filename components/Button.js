f58302d4a9d2cebb2c2d58a046a14e2633b7ad57
rcedenod <rflalejandro2004@gmail.com>
2025-07-16 10:07:56 -0400

    declarando estilos y metodos privados del boton

--- components/Button.js
+++ components/Button.js
@@ -1,39 +1,36 @@
 export class Button {
     // estilos por defecto del boton (si no se le pasan estilos por parametros)
-    defaultStyles = {
+    _defaultStyles = {
         backgroundColor: '#34495E',
         color: 'white',
         padding: '10px 20px',
         border: 'none',
         borderRadius: '5px',
-        cursor: 'pointer',
         fontSize: '16px',
-        fontWeight: 'normal',
-        transition: 'background-color 0.3s ease, transform 0.2s ease'
     };
 
     // hover por defecto del boton (si no se le pasan estilos por parametros)
-    defaultHoverStyles = {
+    _defaultHoverStyles = {
         backgroundColor: '#2C3E50'
     };
 
-    defaultDisabledStyles = {
+    _defaultDisabledStyles = {
         backgroundColor: '#bdc3c742',
         color: '#7f8c8d',
     };
 
-    constructor(parentElement, { text, styles = {}, hoverStyles = {}, disabledStyles = {}, onClick = () => {} }) {
-        if (!(parentElement instanceof HTMLElement)) {
+    constructor(container, { text, styles = {}, hoverStyles = {}, disabledStyles = {}, onClick = () => {} }) {
+        if (!(container instanceof HTMLElement)) {
             throw new Error('Button: el parentElement debe ser un elemento HTML válido.');
         }
 
-        this.parentElement = parentElement;
+        this.container = container;
         this.text = text;
         // fusionamos los dos conjuntos de propiedades, las default y las del usuario
         // el valor de la propiedad que aparece de ultimo en el objeto es el que prevalece
-        this.styles = { ...this.defaultStyles, ...styles };
-        this.hoverStyles = { ...this.defaultHoverStyles, ...hoverStyles };
-        this.disabledStyles = { ...this.defaultDisabledStyles, ...disabledStyles };
+        this.styles = { ...this._defaultStyles, ...styles };
+        this.hoverStyles = { ...this._defaultHoverStyles, ...hoverStyles };
+        this.disabledStyles = { ...this._defaultDisabledStyles, ...disabledStyles };
 
         this.onClick = onClick;
 
@@ -42,7 +39,7 @@ export class Button {
     }
 
     // aplicar los estilos CSS al componente
-    applyStyles(element, stylesObject) {
+    _applyStyles(element, stylesObject) {
         for (const key in stylesObject) {
             if (Object.hasOwnProperty.call(stylesObject, key)) {
                 element.style[key] = stylesObject[key];
@@ -54,7 +51,7 @@ export class Button {
         this.buttonElement.textContent = this.text;
 
         // aplicar estilos base iniciales (cuando no hay hover)
-        this.applyStyles(this.buttonElement, this.styles);
+        this._applyStyles(this.buttonElement, this.styles);
 
         // guardamos los estilos iniciales para volverlos a poner al quitar el hover
         this.initialStyles = {};
@@ -64,13 +61,13 @@ export class Button {
 
         this.handleMouseOver = () => {
             if (!this.buttonElement.disabled) {
-                this.applyStyles(this.buttonElement, this.hoverStyles);
+                this._applyStyles(this.buttonElement, this.hoverStyles);
             }
         };
 
         this.handleMouseOut = () => {
             if (!this.buttonElement.disabled) {
-                this.applyStyles(this.buttonElement, this.initialStyles);
+                this._applyStyles(this.buttonElement, this.initialStyles);
             }
         };
 
@@ -81,7 +78,7 @@ export class Button {
         this.buttonElement.addEventListener('click', this.onClick);
 
         // agregar el boton a su contenedor
-        this.parentElement.appendChild(this.buttonElement);
+        this.container.appendChild(this.buttonElement);
 
         return this.buttonElement;
     }
@@ -100,10 +97,10 @@ export class Button {
             this.buttonElement.disabled = isDisabled;
             if (isDisabled) {
                 // aplicar estilos de deshabilitado
-                this.applyStyles(this.buttonElement, this.disabledStyles);
+                this._applyStyles(this.buttonElement, this.disabledStyles);
             } else {
                 // restaurar estilos por defecto
-                this.applyStyles(this.buttonElement, this.initialStyles);
+                this._applyStyles(this.buttonElement, this.initialStyles);
             }
         }
     }
@@ -111,11 +108,11 @@ export class Button {
     // remover boton del DOM
     remove() {
         // remover si el boton esta creado y si tiene un contenedor o padre
-        if (this.buttonElement && this.buttonElement.parentNode) {
+        if (this.buttonElement && this.buttonElement.parentElement) {
             this.buttonElement.removeEventListener('click', this.onClick);
             this.buttonElement.removeEventListener('mouseover', this.handleMouseOver);
             this.buttonElement.removeEventListener('mouseout', this.handleMouseOut);
-            this.buttonElement.parentNode.removeChild(this.buttonElement);
+            this.buttonElement.parentElement.removeChild(this.buttonElement);
         }
     }
 }
